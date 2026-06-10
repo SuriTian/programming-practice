f20b779bb8e591f1b74b79a4a3bc7a1f771a9b7b
SuriTian <suritian9@gmail.com>
2026-06-09 23:38:39 -0400

    valid parentheses

---
+++ NeetCode150/Stack/valid_parentheses.cpp
@@ -0,0 +1,35 @@
+/**
+ * You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+ * The input string s is valid if and only if:
+ * Every open bracket is closed by the same type of close bracket.
+ * Open brackets are closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * Return true if s is a valid string, and false otherwise.
+ */
+
+#include <string>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> buffer;
+    
+    for (auto& letter : s) {
+        if (buffer.empty()) {
+            buffer.push(letter);
+        }
+        else {
+            if (buffer.top() - letter == -1 || buffer.top() - letter == -2) {
+                buffer.pop();
+            }
+            else {
+                buffer.push(letter);
+            }
+        }
+    }
+
+    return buffer.empty(); 
+    
+}
