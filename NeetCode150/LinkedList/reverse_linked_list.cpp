5d5c6881b5e4fc8bd4f6a77db46f8bcb841b2c76
SuriTian <suritian9@gmail.com>
2026-06-29 22:26:27 -0400

    start linked lists and reverse linked list

---
+++ NeetCode150/LinkedList/reverse_linked_list.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head; 
+        ListNode* prev = nullptr; 
+        ListNode* temp = curr;
+
+        while (curr) {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        head = prev;
+
+        return head; 
+    }
+};
