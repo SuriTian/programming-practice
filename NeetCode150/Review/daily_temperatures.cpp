6f523f26eefc68ccb423e05297cbdc54720b1176
SuriTian <suritian9@gmail.com>
2026-07-11 16:46:27 -0400

    dynamic programmingg soln for daily temp

--- NeetCode150/Review/daily_temperatures.cpp
+++ NeetCode150/Review/daily_temperatures.cpp
@@ -27,3 +27,35 @@ public:
         return res; 
     }
 };
+
+// dynamic proramming ver 
+class Solution2 {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res(temperatures.size());
+
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            if (i == temperatures.size() - 1) res[i] = 0; 
+            else if (temperatures[i] < temperatures[i + 1]) {
+                res[i] = 1; 
+            }
+            else {
+                int toCheck = res[i + 1] + i + 1; 
+
+                // >= is possible because we can have same temperature in the future, so we need to skip it
+                while (toCheck < temperatures.size() && temperatures[i] >= temperatures[toCheck]) {
+                    if (toCheck < res[toCheck] + toCheck) {
+                        toCheck = res[toCheck] + toCheck;
+                    }
+                    else {
+                        break;
+                    }
+                } 
+
+                res[i] = temperatures[toCheck] > temperatures[i] ? toCheck - i : 0; 
+            }
+        }
+
+        return res;
+    }
+};
