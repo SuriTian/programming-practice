5ba8ffaeee062cc75128c5503c06f87a730cd982
SuriTian <suritian9@gmail.com>
2026-07-23 22:13:11 -0400

    valid binary search tree

---
+++ NeetCode150/Trees/valid_binary_search_tree.cpp
@@ -0,0 +1,25 @@
+#include <climits>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool dfs(TreeNode* node, long lower, long upper) {
+        if (!node) return true;
+
+        if (!(lower < node->val && upper > node->val)) return false; 
+
+        return dfs(node->left, lower, node->val) && dfs(node->right, node->val, upper); 
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, LONG_MIN, LONG_MAX);
+    }
+};
