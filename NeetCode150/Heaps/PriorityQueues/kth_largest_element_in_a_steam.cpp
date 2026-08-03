cc71de789c1b05d95e70e4fedd382224fa5cfcf4
SuriTian <suritian9@gmail.com>
2026-08-02 23:27:04 -0400

    second soln to kth largest element in a stream

--- NeetCode150/Heaps/PriorityQueues/kth_largest_element_in_a_steam.cpp
+++ NeetCode150/Heaps/PriorityQueues/kth_largest_element_in_a_steam.cpp
@@ -25,3 +25,31 @@ public:
         return minHeap.top(); 
     }
 };
+
+// solution #2
+class KthLargest {
+public:
+    priority_queue<int> minHeap; 
+    int k;
+    
+    KthLargest(int k, vector<int>& nums) {
+       this->k = k; 
+       for (int i = 0; i < nums.size(); i++) {
+            minHeap.push(-1 * nums[i]);
+            
+            if (minHeap.size() > k) {
+                minHeap.pop(); 
+            }
+       } 
+    }
+    
+    int add(int val) {
+        minHeap.push(-1 * val);
+
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        } 
+
+        return minHeap.top() * -1;
+    }
+};
