bb24061f35b21d56f9ccb84c986256fbf7a7d2df
SuriTian <suritian9@gmail.com>
2026-08-15 21:43:34 -0400

    started Graphs; number of islands

---
+++ NeetCode150/Graphs/number_of_islands.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int count = 0; 
+
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        if (r >= grid.size() || c >= grid[0].size() || grid[r][c] == '0') return;
+
+        grid[r][c] = '0'; 
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c - 1); 
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        for (int r = 0; r < grid.size(); r++) {
+            for (int c = 0; c < grid[0].size(); c++) {
+                if (grid[r][c] == '1') {
+                    count++; 
+                    dfs(grid, r, c); 
+                }
+            }
+        }
+
+        return count; 
+    }
+};
